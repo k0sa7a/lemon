b557ced7593bf1ecf49c85b4dbc3854a6fd815ca
Stefan Filev <stefan.s.filev@gmail.com>
2022-05-28 12:52:09 +0100

    custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :locations
+  get '/404', to: 'errors#not_found'
+  get '/500', to: 'errors#internal_server'
 end
