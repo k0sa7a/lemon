cd5a44b04bac5005f83a3de8ad101c3e91e6a91a
Ashley Lampard <ashleylampard@live.com>
2022-06-01 13:00:37 +0100

    matching is working with draw

--- app/javascript/controllers/mapbox_matching_controller.js
+++ app/javascript/controllers/mapbox_matching_controller.js
@@ -13,7 +13,6 @@ export default class extends Controller {
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
     mapboxgl.center = [-122.42136449,37.80176523]
-    console.log(this)
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v11",
@@ -31,98 +30,97 @@ export default class extends Controller {
 
 
     // Use the coordinates you drew to make the Map Matching API request
-  function updateRoute() {
-    // Set the profile
-    const profile = 'driving';
-    console.log('here');
-    // Get the coordinates that were drawn on the map
-    const data = draw.getAll();
-    const lastFeature = data.features.length - 1;
-    const coords = data.features[lastFeature].geometry.coordinates;
-    // Format the coordinates
-    const newCoords = coords.join(';');
-    // Set the radius for each coordinate pair to 25 meters
-    const radius = coords.map(() => 25);
+    function updateRoute() {
+      // Set the profile
+      const profile = 'driving';
+      // Get the coordinates that were drawn on the map
+      const data = draw.getAll();
+      const lastFeature = data.features.length - 1;
+      const coords = data.features[lastFeature].geometry.coordinates;
+      console.log(coords)
+      // Format the coordinates
+      const newCoords = coords.join(';');
+      console.log(newCoords)
+      // Set the radius for each coordinate pair to 25 meters
+      const radius = coords.map(() => 25);
+      console.log(radius)
 
-    async function getMatch(coordinates, radius, profile) {
-      // Separate the radiuses with semicolons
-      const radiuses = radius.join(';');
-      // Create the query
-      const query = await fetch(
-        `https://api.mapbox.com/matching/v5/mapbox/${profile}/${coordinates}?geometries=geojson&radiuses=${radiuses}&steps=true&access_token=${mapboxgl.accessToken}`,
-        { method: 'GET' }
-      );
-      const response = await query.json();
-      // Handle errors
-      if (response.code !== 'Ok') {
-        alert(
-          `${response.code} - ${response.message}.\n\nFor more information: https://docs.mapbox.com/api/navigation/map-matching/#map-matching-api-errors`
+      async function getMatch(coordinates, radius, profile) {
+        // Separate the radiuses with semicolons
+        const radiuses = radius.join(';');
+        // Create the query
+        const query = await fetch(
+          `https://api.mapbox.com/matching/v5/mapbox/${profile}/${coordinates}?geometries=geojson&radiuses=${radiuses}&steps=true&access_token=${mapboxgl.accessToken}`,
+          { method: 'GET' }
         );
-        return;
-      }
-      // Get the coordinates from the response
-      const coords = response.matchings[0].geometry;
-      console.log(coords);
-      // Draw the route on the map
-      // addRoute(coords);
-
-
-      // Draw the Map Matching route as a new layer on the map
-      function addRoute(coords) {
-        // If a route is already loaded, remove it
-        if (self.map.getSource('route')) {
-          self.map.removeLayer('route');
-          self.map.removeSource('route');
-        } else {
-          // Add a new layer to the map
-          self.map.addLayer({
-            id: 'route',
-            type: 'line',
-            source: {
-              type: 'geojson',
-              data: {
-                type: 'Feature',
-                properties: {},
-                geometry: coords
-              }
-            },
-            layout: {
-              'line-join': 'round',
-              'line-cap': 'round'
-            },
-            paint: {
-              'line-color': '#03AA46',
-              'line-width': 8,
-              'line-opacity': 0.8
-            }
-          });
+        const response = await query.json();
+        // Handle errors
+        if (response.code !== 'Ok') {
+          alert(
+            `${response.code} - ${response.message}.\n\nFor more information: https://docs.mapbox.com/api/navigation/map-matching/#map-matching-api-errors`
+          );
+          return;
         }
-      }
+        // Get the coordinates from the response
+        const coords = response.matchings[0].geometry;
+        // Draw the route on the map
+        // addRoute(coords);
 
-      function getInstructions(data) {
-        // Target the sidebar to add the instructions
-        const directions = document.getElementById('directions');
-        let tripDirections = '';
-        // Output the instructions for each step of each leg in the response object
-        for (const leg of data.legs) {
-          const steps = leg.steps;
-          for (const step of steps) {
-            tripDirections += `<li>${step.maneuver.instruction}</li>`;
+
+        // Draw the Map Matching route as a new layer on the map
+        function addRoute(coords) {
+          // If a route is already loaded, remove it
+          if (self.map.getSource('route')) {
+            self.map.removeLayer('route');
+            self.map.removeSource('route');
+          } else {
+            // Add a new layer to the map
+            self.map.addLayer({
+              id: 'route',
+              type: 'line',
+              source: {
+                type: 'geojson',
+                data: {
+                  type: 'Feature',
+                  properties: {},
+                  geometry: coords
+                }
+              },
+              layout: {
+                'line-join': 'round',
+                'line-cap': 'round'
+              },
+              paint: {
+                'line-color': '#03AA46',
+                'line-width': 8,
+                'line-opacity': 0.8
+              }
+            });
           }
         }
-        directions.innerHTML = `<p><strong>Trip duration: ${Math.floor(
-          data.duration / 60
-        )} min.</strong></p><ol>${tripDirections}</ol>`;
+
+        function getInstructions(data) {
+          // Target the sidebar to add the instructions
+          const directions = document.getElementById('directions');
+          let tripDirections = '';
+          // Output the instructions for each step of each leg in the response object
+          for (const leg of data.legs) {
+            const steps = leg.steps;
+            for (const step of steps) {
+              tripDirections += `<li>${step.maneuver.instruction}</li>`;
+            }
+          }
+          directions.innerHTML = `<p><strong>Trip duration: ${Math.floor(
+            data.duration / 60
+          )} min.</strong></p><ol>${tripDirections}</ol>`;
+        }
+
+        addRoute(coords);
+        getInstructions(response.matchings[0]);
       }
 
-      addRoute(coords);
-      getInstructions(response.matchings[0]);
+      getMatch(newCoords, radius, profile);
     }
-
-    getMatch(newCoords, radius, profile);
-
-
-  }
 
 
     // If the user clicks the delete draw button, remove the layer if it exists
@@ -132,22 +130,10 @@ export default class extends Controller {
       self.map.removeSource('route');
     }
 
-
-
-
-
-
-
-
     this.map.on('draw.create', updateRoute);
     this.map.on('draw.update', updateRoute);
     this.map.on('draw.delete', removeRoute);
-
   }
-
-
-
-
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
@@ -174,59 +160,7 @@ export default class extends Controller {
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  async getMatch(coordinates, radius, profile) {
-    // Separate the radiuses with semicolons
-    const radiuses = radius.join(';');
-    // Create the query
-    const query = await fetch(
-      `https://api.mapbox.com/matching/v5/mapbox/${profile}/${coordinates}?geometries=geojson&radiuses=${radiuses}&steps=true&access_token=${mapboxgl.accessToken}`,
-      { method: 'GET' }
-    );
-    const response = await query.json();
-    // Handle errors
-    if (response.code !== 'Ok') {
-      alert(
-        `${response.code} - ${response.message}.\n\nFor more information: https://docs.mapbox.com/api/navigation/map-matching/#map-matching-api-errors`
-      );
-      return;
-    }
-    // Get the coordinates from the response
-    const coords = response.matchings[0].geometry;
-    console.log(coords);
-    // Draw the route on the map
-    // addRoute(coords);
-  }
-
-
-
-
-
-
-
-
-
-
-
 }
-
 
 const draw = new MapboxDraw({
   // Instead of showing all the draw tools, show only the line string and delete tools.
@@ -286,39 +220,3 @@ const draw = new MapboxDraw({
     }
   ]
 });
-
-
-// Make a Map Matching request
-
-
-// Draw the Map Matching route as a new layer on the map
-function addRoute(coords) {
-  // If a route is already loaded, remove it
-  if (map.getSource('route')) {
-    map.removeLayer('route');
-    map.removeSource('route');
-  } else {
-    // Add a new layer to the map
-    map.addLayer({
-      id: 'route',
-      type: 'line',
-      source: {
-        type: 'geojson',
-        data: {
-          type: 'Feature',
-          properties: {},
-          geometry: coords
-        }
-      },
-      layout: {
-        'line-join': 'round',
-        'line-cap': 'round'
-      },
-      paint: {
-        'line-color': '#03AA46',
-        'line-width': 8,
-        'line-opacity': 0.8
-      }
-    });
-  }
-}
