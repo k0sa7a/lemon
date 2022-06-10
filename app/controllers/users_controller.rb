1b8c6152d43334d3814f6f62e502806361c110a2
Doreen Lam <doreenlam95@gmail.com>
2022-06-10 21:24:53 +0100

    start to adding calendar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,13 @@ class UsersController < ApplicationController
     redirect_to user_path(@user)
   end
 
+  def index
+    # Scope your query to the dates being shown:
+    start_date = params.fetch(:start_date, Date.today).to_date
+    # For a monthly view:
+    @meetings = Meeting.where(starts_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+  end
+
   private
 
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :locations
   has_many :itineraries
   has_one_attached :photo
-  has_many :meetings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
