c39952142edf6ca568b689e38129cde89f411219
Ashley Lampard <ashleylampard@live.com>
2022-06-23 08:16:14 +0100

    added navigation buttons to the chatrooms and removed the blue glow on forms when in focus

--- app/javascript/controllers/location_modal_controller.js
+++ app/javascript/controllers/location_modal_controller.js
@@ -41,7 +41,7 @@ export default class extends Controller {
           confirmButtonText: "Cool",
         });
       });
-    this.cancelForm();
+    this.cancelForm(event);
   }
 
   addItemToItinerary(event) {
--- app/models/privatechat.rb
+++ app/models/privatechat.rb
@@ -1,4 +1,5 @@
 class Privatechat < ApplicationRecord
   belongs_to :user
+  belongs_to :coach
   belongs_to :chatroom
 end
